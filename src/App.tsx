2e9961ab3372a6fe5b47da204818e6902a30d012
fanispoulinakisai-boop <fanispoulinakisai@gmail.com>
2026-02-24 19:58:05 +0200

    Update src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from '@/components/ui/sonner';
+import Hero from './sections/Hero';
+import WhatThisIs from './sections/WhatThisIs';
+import Plans from './sections/Plans';
+import Different from './sections/Different';
+import Work from './sections/Work';
+import Brands from './sections/Brands';
+import FAQ from './sections/FAQ';
+import Contact from './sections/Contact';
+import Footer from './sections/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-cream">
+      <Hero />
+      <WhatThisIs />
+      <Plans />
+      <Different />
+      <Work />
+      <Brands />
+      <FAQ />
+      <Contact />
+      <Footer />
+      <Toaster position="bottom-right" />
+    </div>
+  );
+}
+
+export default App;
